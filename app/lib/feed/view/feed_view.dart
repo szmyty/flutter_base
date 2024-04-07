1b5affbda1625431fd65708259f1d64a03d09f80
szmyty <szmyty@gmail.com>
2024-04-07 16:09:45 -0400

    Update.

--- app/lib/feed/view/feed_view.dart
+++ app/lib/feed/view/feed_view.dart
@@ -1,3 +1,4 @@
+import "package:app/categories/categories.dart";
 import "package:app/feed/bloc/feed_bloc.dart";
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
